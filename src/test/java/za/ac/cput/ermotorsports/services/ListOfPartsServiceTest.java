b2bd8a76522ec10b7183c35543e4a463dd22e1ce
212023535 <rdzeeman@gmail.com>
2015-10-21 13:50:10 +0200

    Created Service test for List of parts

---
+++ src/test/java/za/ac/cput/ermotorsports/services/ListOfPartsServiceTest.java
@@ -0,0 +1,89 @@
+package za.ac.cput.ermotorsports.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.testng.annotations.Test;
+import org.junit.Assert;
+import za.ac.cput.ermotorsports.App;
+import za.ac.cput.ermotorsports.conf.factory.ListOfPartFactory;
+import za.ac.cput.ermotorsports.domain.ListOfParts;
+import za.ac.cput.ermotorsports.service.ListOfPartsService;
+
+/**
+ * Created by Rudi.Zeeman on 21.10.15.
+ */
+@SpringApplicationConfiguration(classes = App.class)
+@WebAppConfiguration
+public class ListOfPartsServiceTest  extends AbstractTestNGSpringContextTests
+{
+    private Long id;
+
+    @Autowired
+    ListOfPartsService service;
+
+    @Test
+    public void create() throws Exception
+    {
+        Long car = null;
+        Long engine = null;
+        Long extra = 12345678910L;
+
+        //Create a List of Parts
+        ListOfParts listOfParts = ListOfPartFactory.createListOfParts(car,engine,extra);
+
+        //Save in the database
+        service.save(listOfParts);
+
+        //ID should be available
+        id = listOfParts.getId();
+
+        Assert.assertNotNull(id);
+    }
+
+    @Test(dependsOnMethods = "create")
+    public void read() throws Exception
+    {
+        //Get list of parts
+        ListOfParts listOfParts = service.findById(id);
+
+        Assert.assertNotNull(id);
+    }
+
+    @Test(dependsOnMethods = "read")
+    public void update() throws Exception
+    {
+        Long car = null;
+        Long engine = null;
+        Long extra = 12345678910L;
+
+        //Get list of parts
+        ListOfParts listOfParts = service.findById(id);
+
+        //Change it
+        ListOfParts newListOfParts = new ListOfParts.Build(listOfParts.getId()).copy(listOfParts).partID(1234567888L).build();
+
+        //Save it
+        service.update(newListOfParts);
+
+        //Get updated LOP
+        ListOfParts updatedListOfParts = service.findById(id);
+
+        Assert.assertNotNull(id);
+    }
+
+    @Test(dependsOnMethods = "update")
+    public void delete() throws Exception
+    {
+        //Get list of parts
+        ListOfParts listOfParts = service.findById(id);
+
+        service.delete(listOfParts);
+
+        ListOfParts deletedLOP = service.findById(id);
+
+        Assert.assertNull(deletedLOP);
+    }
+}
