c1ed9ad76ea41cd5f19120992806cc878001d5a5
Edmund Simons <edmundsimons69@gmail.com>
2015-10-23 11:30:43 +0200

    Updated some stuff

--- src/main/java/za/ac/cput/ermotorsports/api/TransactionPage.java
+++ src/main/java/za/ac/cput/ermotorsports/api/TransactionPage.java
@@ -1,6 +1,8 @@
 package za.ac.cput.ermotorsports.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,13 +24,13 @@ public class TransactionPage
     @RequestMapping(value = "transaction",method = RequestMethod.GET)
     public String Index(){return "This is a transaction page";}
 
-    @RequestMapping(value = "/transaction",method = RequestMethod.GET)
-    public Transaction getTransaction(){
-        Transaction transaction = new Transaction.Build("R10000").car(1L).customer(2L).transNo(3L).build();
-
-        return transaction;
+    @RequestMapping(value = "/transaction/",method = RequestMethod.GET)
+    public ResponseEntity<List<Transaction>> listAllTransactions(){
+        List<Transaction> transaction = service.findAll();
+        if(transaction.isEmpty())
+        {
+            return new ResponseEntity<List<Transaction>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<Transaction>>(transaction, HttpStatus.OK);
     }
-
-    @RequestMapping(value = "transactions",method = RequestMethod.GET)
-    public List<Transaction> getTransactions(){return service.getTransactions();}
 }
--- src/main/java/za/ac/cput/ermotorsports/service/Impl/TransactionServiceImpl.java
+++ src/main/java/za/ac/cput/ermotorsports/service/Impl/TransactionServiceImpl.java
@@ -1,6 +1,7 @@
 package za.ac.cput.ermotorsports.service.Impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import za.ac.cput.ermotorsports.domain.Transaction;
 import za.ac.cput.ermotorsports.repository.TransactionRepository;
 import za.ac.cput.ermotorsports.service.TransactionService;
@@ -11,12 +12,34 @@ import java.util.List;
 /**
  * Created by Edmund.Simons on 15/10/2015.
  */
+@Service
 public class TransactionServiceImpl implements TransactionService
 {
     @Autowired
     private TransactionRepository repository;
 
-    public List<Transaction> getTransactions() {
+    @Override
+    public Transaction findById(Long id) {
+        return repository.findOne(id);
+    }
+
+    @Override
+    public Transaction save(Transaction entity) {
+        return repository.save(entity);
+    }
+
+    @Override
+    public Transaction update(Transaction entity) {
+        return repository.save(entity);
+    }
+
+    @Override
+    public void delete(Transaction entity) {
+        repository.delete(entity);
+    }
+
+    @Override
+    public List<Transaction> findAll() {
         List<Transaction> allTransactions = new ArrayList<Transaction>();
         Iterable<Transaction> transactions = repository.findAll();
         for(Transaction transaction:transactions)
